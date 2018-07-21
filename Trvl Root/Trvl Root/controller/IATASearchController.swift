ada7385cb45ca8f891946f65c95bf93943175ad7
Jason Sanchez <sanchezjason@me.com>
2018-07-21 03:50:14 -0600

    SplashView

--- Trvl Root/Trvl Root/controller/IATASearchController.swift
+++ Trvl Root/Trvl Root/controller/IATASearchController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+import RevealingSplashView
 
-class IATASearchController: UIViewController {
+class IATASearchController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var iataCodeField: UITextField!
+    
+    var airportAPI = AirportAPI()
+    
+    let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        self.view.addSubview(revealingSplashView)
+        revealingSplashView.animationType = SplashAnimationType.heartBeat
+        revealingSplashView.startAnimation()
+        
+        revealingSplashView.heartAttack = true
     }
     
-
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        title = "Destination"
     }
-
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        iataCodeField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func backgroundTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func searchAirportPressed(_ sender: UIButton) {
+        if let destination = iataCodeField.text {
+            airportAPI.retrieveAirport(destination)
+        }
+    }
 }
